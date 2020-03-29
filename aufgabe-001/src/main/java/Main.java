c063f7b7f463541de9951c3ebf0d5aee99a3e82a
Christian Wansart <christian.wansart@openknowledge.de>
2020-03-29 17:57:37 +0200

    Initial commit

---
+++ aufgabe-001/src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // TODO: Schreibe ein Programm, dass über System.out.println "Hello World" printed.
+        System.out.println("Hello World");
+    }
+}
