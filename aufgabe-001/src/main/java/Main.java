7e7f6d855beb45ea6763a75bb7516899ccc5a1dc
Christian Wansart <christian.wansart@openknowledge.de>
2020-04-07 13:37:55 +0200

    Fertig

--- aufgabe-001/src/main/java/Main.java
+++ aufgabe-001/src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         // TODO: Schreibe ein Programm, dass über System.out.println "Hello World" printed.
-        System.out.println("Hello World");
+        System.out.println("Hello Worasdasdasdld");
     }
 }
